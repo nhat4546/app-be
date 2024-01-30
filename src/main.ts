e64a7c3ef7a9946562e419270670240f9a0bf8ef
Sotatek-Nhatnguyen2 <nhat.nguyen2@sotatek.com>
2024-01-30 14:38:35 +0700

    feat: env, docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configDocument = new DocumentBuilder()
+    .setTitle('Api Documentation')
+    .setDescription('App API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, configDocument);
+  SwaggerModule.setup('/api', app, document);
+
+  const config = app.get(ConfigService);
+  app.enableCors({
+    origin: config.get('PUBLIC_URL'),
+    credentials: true,
+  });
+
+  const port = +config.get('PORT') || 5000;
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(port, () => {
+    console.log('App listen port ' + port);
+  });
 }
 bootstrap();
